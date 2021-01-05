fc34910a91b3e724651cb2ef4b0acd51f482628e
kaytundreia <74748908+kaytundreia@users.noreply.github.com>
2021-01-04 21:27:49 -0600

    add json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,10 @@ export class AppComponent {
       response.json().then(function(data) {
         let fetchedSatellites = data.satellites;
         for(let i=0;i<fetchedSatellites.length; i++) {
-          
+          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellite)
         }
-      },bind(this));
+      }.bind(this));
     }.bind(this));
   }
 }
